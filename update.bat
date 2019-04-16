837b7a88e03c95e45832cf5621a85ff9e53e98f2
cydams <mariosmsk@yahoo.com>
2019-04-16 14:21:04 +0300

    update

--- update.bat
+++ update.bat
@@ -1,2 +1,3 @@
+taskkill /im git-cmd.exe /f
 cd Cyprus-Weather
 "C:\Program Files\Git\git-cmd.exe" "C:\Users\mkiria01\OneDrive - University of Cyprus\Wordpress - Vroxes - Data\Cyprus-Weather\run.sh"
